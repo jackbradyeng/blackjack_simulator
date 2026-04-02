7d89ef0a5c3b36bace804e259ddd8c63f68eb587
jack-brady <jackbradyx@proton.me>
2026-04-02 18:53:54 +1000

    Switch from JDK version from 25 to 21

--- src/main/java/org/example/blackjack_emulator/Launcher.java
+++ src/main/java/org/example/blackjack_emulator/Launcher.java
@@ -4,7 +4,7 @@ import Controller.Controller;
 import static Model.Constants.*;
 
 public class Launcher {
-    void main(String[] args) {
+    public static void main(String[] args) {
         Controller controller = new Controller(DEFAULT_NUMBER_OF_PLAYERS, DEFAULT_NUMBER_OF_DECKS, false);
         controller.startGame();
     }
