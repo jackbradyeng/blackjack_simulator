3f3fba46da4613db5afb41cc666a1086e2dc582d
jack-brady <jackbradyx@proton.me>
2026-04-02 19:00:16 +1000

    Remove module info

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.blackjack_emulator {
-    requires java.sql;
-    requires java.desktop;
-    requires jdk.jfr;
-    requires static org.junit.jupiter.api;
-    // requires org.example.blackjack_emulator;
-    opens org.example.blackjack_emulator;
-    exports org.example.blackjack_emulator;
-}
