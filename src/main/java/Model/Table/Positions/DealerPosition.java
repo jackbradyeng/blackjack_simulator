f38a8cbefc984b39aebf0a2398671d0249664eb0
jack-brady <jackbradyx@proton.me>
2026-04-06 01:55:59 +1000

    Apply getter and setter annotations to dealer position

--- src/main/java/Model/Table/Positions/DealerPosition.java
+++ src/main/java/Model/Table/Positions/DealerPosition.java
@@ -1,9 +1,13 @@
 package Model.Table.Positions;
 
 import Model.Table.Hands.DealerHand;
+import lombok.Getter;
+import lombok.Setter;
 
 public class DealerPosition {
 
+    @Getter
+    @Setter
     private DealerHand hand;
 
     public DealerPosition() {
@@ -12,13 +16,5 @@ public class DealerPosition {
 
     public void clearHand() {
         hand.getCards().clear();
-    }
-
-    public DealerHand getHand() {
-        return hand;
-    }
-
-    public void setHand(DealerHand hand) {
-        this.hand = hand;
     }
 }
