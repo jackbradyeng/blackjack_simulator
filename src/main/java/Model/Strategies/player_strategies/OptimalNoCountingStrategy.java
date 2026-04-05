c2acb8635c44bc901ee8c6612c1ff416240cfaab
jack-brady <jackbradyx@proton.me>
2026-04-05 15:34:32 +1000

    Refactor optimal strategy execute method

--- src/main/java/Model/Strategies/player_strategies/OptimalNoCountingStrategy.java
+++ src/main/java/Model/Strategies/player_strategies/OptimalNoCountingStrategy.java
@@ -15,18 +15,24 @@ public class OptimalNoCountingStrategy implements PlayerStrategy {
 
     /** mathematically optimal blackjack strategy without counting cards. */
     private String optimalStrategy(PlayerHand playerHand, DealerHand dealerHand) {
-        // if the two player cards are equal in value, first test to see if the action is a split
+
         if(playerHand.hasSplitOption()) {
-            String action = OptimalStrategyUtility.executeSplittingStrategy(playerHand, dealerHand);
-            if (action.equals(NO_SPLIT)) {
-                return OptimalStrategyUtility.executeHardValuesStrategy(playerHand, dealerHand);
-            } else {
-                return action;
-            }
+            return handleSplittingCase(playerHand, dealerHand);
+
         } else if(playerHand.hasAce() && playerHand.getCards().size() == 2) {
                 return OptimalStrategyUtility.executeSoftValuesStrategy(playerHand, dealerHand);
+
         } else {
             return OptimalStrategyUtility.executeHardValuesStrategy(playerHand, dealerHand);
+        }
+    }
+
+    private String handleSplittingCase(PlayerHand playerHand, DealerHand dealerHand) {
+        String action = OptimalStrategyUtility.executeSplittingStrategy(playerHand, dealerHand);
+        if (action.equals(NO_SPLIT)) {
+            return OptimalStrategyUtility.executeHardValuesStrategy(playerHand, dealerHand);
+        } else {
+            return action;
         }
     }
 }
