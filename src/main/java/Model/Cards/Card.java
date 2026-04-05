3073208b0105df7f16e7a71051fc3121557b7d88
jack-brady <jackbradyx@proton.me>
2026-04-05 17:03:24 +1000

    Apply data & constructor annotations to card

--- src/main/java/Model/Cards/Card.java
+++ src/main/java/Model/Cards/Card.java
@@ -1,33 +1,17 @@
 package Model.Cards;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Card {
 
-    String name;
-    int value;
-
-    public Card(String name, int value) {
-        this.name = name;
-        this.value = value;
-    }
+    private String name;
+    private int value;
 
     @Override
     public String toString() {
         return name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
     }
 }
