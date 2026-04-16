7e4dda6db0f8d260b43fa28e2dc2f9e2caafcb87
jack-brady <jackbradyx@proton.me>
2026-04-16 13:51:46 +1000

    Add double bet processor interface

---
+++ src/main/java/Model/Table/Processors/DoubleBetProcessorInterface.java
@@ -0,0 +1,10 @@
+package Model.Table.Processors;
+
+import Model.Actors.Player;
+import Model.Table.Hands.PlayerHand;
+import Model.Table.Positions.PlayerPosition;
+
+public interface DoubleBetProcessorInterface {
+
+    void process(Player player, PlayerPosition playerPosition, PlayerHand playerHand);
+}
