cc4f3e1b8cc642dc3516bcf91c01c22a0e75d698
jack-brady <jackbradyx@proton.me>
2026-04-17 12:50:03 +1000

    Add split bet processor interface

---
+++ src/main/java/Model/Table/Processors/SplitBetProcessorInterface.java
@@ -0,0 +1,14 @@
+package Model.Table.Processors;
+
+import Model.Actors.Player;
+import Model.Table.Hands.PlayerHand;
+import Model.Table.Positions.PlayerPosition;
+import java.util.ArrayList;
+
+public interface SplitBetProcessorInterface {
+
+    void process(Player player,
+                        PlayerPosition playerPosition,
+                        PlayerHand playerHand,
+                        ArrayList<PlayerHand> activeHands);
+}
