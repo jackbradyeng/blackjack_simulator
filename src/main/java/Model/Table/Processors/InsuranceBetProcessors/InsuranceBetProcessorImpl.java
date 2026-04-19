a0e711d4c5659e30eb8fb214396656429d6af131
jack-brady <jackbradyx@proton.me>
2026-04-19 12:04:25 +1000

    Simplify insurance bet processor

--- src/main/java/Model/Table/Processors/InsuranceBetProcessors/InsuranceBetProcessorImpl.java
+++ src/main/java/Model/Table/Processors/InsuranceBetProcessors/InsuranceBetProcessorImpl.java
@@ -11,16 +11,11 @@ import lombok.NoArgsConstructor;
 public class InsuranceBetProcessorImpl implements InsuranceBetProcessor {
 
     public void process(Player player, PlayerPosition playerPosition, double amount) {
-        bookInsuranceBet(player, playerPosition, amount);
+        bookBet(player, playerPosition, amount);
     }
 
     /** books an insurance bet for a player on a given position for a given amount. To be called AFTER the cards are
      * dealt. */
-    private void bookInsuranceBet(Player player, PlayerPosition position, double amount) {
-        bookBet(player, position, amount);
-    }
-
-    /** books an insurance bet for a player on a given position for a given amount. */
     private void bookBet(Player player, PlayerPosition position, double amount) {
         InsuranceBet iBet = new InsuranceBet(amount);
         Map.Entry<Player, Bet> entry = Map.entry(player, iBet);
