6f85e24769236f80472421e028b8462bfc563f95
jack-brady <jackbradyx@proton.me>
2026-04-20 22:03:55 +1000

    Add simulation statistics to table stats

--- src/main/java/Model/Observers/TableStats.java
+++ src/main/java/Model/Observers/TableStats.java
@@ -2,23 +2,22 @@ package Model.Observers;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import static Model.Constants.DEFAULT_PLAYER_BET_AMOUNT;
+import static Model.Constants.DEFAULT_PLAYER_STARTING_CHIPS;
 
 @Data
 @NoArgsConstructor
 public class TableStats {
 
-    public int handCount = 0;
-    public int splitCount = 0;
-    public int blackjackCount = 0;
-    public int playerWinCount = 0;
-    public int playerLossCount = 0;
-    public int pushCount = 0;
     private int handCount = 0;
     private int splitCount = 0;
     private int blackjackCount = 0;
     private int playerWinCount = 0;
     private int playerLossCount = 0;
     private int pushCount = 0;
+    private double runningProfit = 0;
+    private double profitPerHand = 0;
+    private double expectedValuePerHand = 0;
 
     public void incrementHandCount() { handCount++; }
     public void incrementSplitCount() { splitCount++; }
@@ -26,4 +25,7 @@ public class TableStats {
     public void incrementPlayerWinCount() { playerWinCount++; }
     public void incrementPlayerLossCount() { playerLossCount++; }
     public void incrementPushCount() { pushCount++; }
+    public void setRunningProfit(double playerChips) { this.runningProfit = playerChips - DEFAULT_PLAYER_STARTING_CHIPS; }
+    public void setProfitPerHand(int handNumber) {this.profitPerHand = this.runningProfit / ((double) handNumber); }
+    public void setExpectedValuePerHand() {this.expectedValuePerHand = this.profitPerHand / DEFAULT_PLAYER_BET_AMOUNT; }
 }
