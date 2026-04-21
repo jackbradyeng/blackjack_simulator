b71531b39c301fa4ca2f4b7045c7d64d200c9e0b
jack-brady <jackbradyx@proton.me>
2026-04-21 14:27:23 +1000

    Move simulation mode logic to orchestrator class

---
+++ src/main/java/Model/Orchestrators/SimulationModeOrchestrator.java
@@ -0,0 +1,38 @@
+package Model.Orchestrators;
+
+import Model.Actors.Player;
+import Model.Observers.TablePrinter;
+import Model.Observers.TableStats;
+import Model.Table.Table;
+import java.time.Duration;
+import java.time.Instant;
+import static Model.Constants.DEFAULT_NUMBER_OF_ITERATIONS;
+import static Model.Constants.DEFAULT_PLAYER_BET_AMOUNT;
+
+public class SimulationModeOrchestrator implements GameModeOrchestrator {
+
+    @Override
+    public void runGame(Table table, TablePrinter tablePrinter, TableStats tableStats) {
+        Instant start = Instant.now();
+        tablePrinter.printWelcomeMessage();
+        Player mainPlayer = table.getPlayers().getFirst();
+
+        for (int i = 0; i < DEFAULT_NUMBER_OF_ITERATIONS; i++) {
+            table.startupRoutine();
+            table.getBettingService().bookStandardBet(mainPlayer, mainPlayer.getDefaultPosition(), DEFAULT_PLAYER_BET_AMOUNT);
+            table.drawRoutine();
+            table.executePlayerStrategyForAll();
+            if ((i + 1) % 1000 == 0) { tablePrinter.printDealerHand(); }
+            table.executeDealerStrategy();
+            table.windDownRoutine();
+            tableStats.setRunningProfit(mainPlayer.getChips());
+            tableStats.setProfitPerHand(i + 1);
+            tableStats.setExpectedValuePerHand();
+            if ((i + 1) % 1000 ==0) { tablePrinter.printStatistics(i + 1, tableStats); }
+        }
+        Instant end = Instant.now();
+        Duration timeElapsed = Duration.between(start, end);
+        long seconds = timeElapsed.getSeconds();
+        System.out.println("Total processing time: " + seconds + " seconds.");
+    }
+}
