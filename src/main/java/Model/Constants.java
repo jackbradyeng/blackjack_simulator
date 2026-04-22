b97e96b79879182b8dd08fcf65ced594fdca014f
jack-brady <jackbradyx@proton.me>
2026-04-22 21:39:57 +1000

    Update constants file

--- src/main/java/Model/Constants.java
+++ src/main/java/Model/Constants.java
@@ -4,15 +4,18 @@ public final class Constants {
 
     public Constants() {}
 
-    // actor constants
+    // ACTOR CONSTANTS
     public static final int DEFAULT_NUMBER_OF_PLAYERS = 1;
     public static final int DEFAULT_PLAYER_STARTING_CHIPS = 500;
     public static final int DEFAULT_DEALER_STARTING_CHIPS = 15000;
     public static final int DEFAULT_DEALER_DRAW_VALUE = 17;
 
-    // strategy constants
+    // STRATEGY CONSTANTS
     public static final double DEFAULT_PLAYER_BET_AMOUNT = 25;
     public static final double DEFAULT_PLAYER_INSURANCE_BET = 10;
+
+    // PLAYER COMMANDS
+    public static final String QUIT = "QUIT";
     public static final String HIT = "HIT";
     public static final String STAND = "STAND";
     public static final String DOUBLE = "DOUBLE";
@@ -20,7 +23,7 @@ public final class Constants {
     public static final String NO_SPLIT = "NO SPLIT";
     public static final String INSURANCE = "INSURANCE";
 
-    // deck constants
+    // DECK CONSTANTS
     public static final int DEFAULT_NUMBER_OF_DECKS = 4;
     public static final int BLACKJACK_CONSTANT = 21;
     public static final int NUMBER_OF_SUITS = 4;
@@ -29,11 +32,11 @@ public final class Constants {
     public static final int NEW_DECK_THRESHOLD = 52;
     public static final String ACE = "Ace";
 
-    // card value constants
+    // CARD VALUE CONSTANTS
     public static final int ACE_LOWER_VALUE = 1;
     public static final int ACE_UPPER_VALUE = 11;
 
-    // table constants
+    // TABLE CONSTANTS
     public static final int DEFAULT_TABLE_POSITIONS = 5; // excludes the dealer
     public static final int DEFAULT_MIN_BET_SIZE = 25;
     public static final int DEFAULT_PAYOUT_RATIO = 1;
@@ -41,9 +44,7 @@ public final class Constants {
     public static final int DEFAULT_BLACKJACK_PAYOUT_NUMERATOR = 2;
     public static final int DEFAULT_INSURANCE_RATIO = 3;
 
-    // simulation constants
-    public static final int DEFAULT_NUMBER_OF_ITERATIONS = 100_000;
-
-    // command line constants
-    public static final int DEFAULT_COUNTDOWN_TIME = 1000;
+    // SIMULATION CONSTANTS
+    public static final int DEFAULT_NUMBER_OF_ITERATIONS = 500_000;
+    public static final int DEFAULT_COUNTDOWN_TIME = 1_000;
 }
