fa946fc2ebd9f15a0496d90b9f57c46efb05705f
jack-brady <jackbradyx@proton.me>
2026-04-24 21:29:23 +1000

    Add dealer strategy orchestrator

---
+++ src/main/java/Model/Orchestrators/actor_strategy_orchestrators/DealerStrategyOrchestrator.java
@@ -0,0 +1,17 @@
+package Model.Orchestrators.actor_strategy_orchestrators;
+
+import Model.Observers.TablePrinter;
+import Model.Table.Table;
+import java.util.Objects;
+import static Model.Constants.STAND;
+
+public class DealerStrategyOrchestrator {
+
+    /** executes the dealer's strategy. */
+    public void executeDealerStrategy(Table table, TablePrinter tablePrinter) {
+        while(!Objects.equals(table.getDealer().executeStrategy(), STAND)) {
+            table.handleDealerAction(table.getDealer().executeStrategy());
+        }
+        tablePrinter.printDealerHand();
+    }
+}
