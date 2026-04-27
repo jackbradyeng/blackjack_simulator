262eace8d0a90fe8fa53f9c05d76c29c4c77443a
jack-brady <jackbradyx@proton.me>
2026-04-27 22:47:38 +1000

    Refactor existing deck unit tests

--- src/test/java/DeckTesting.java
+++ src/test/java/DeckTesting.java
@@ -1,35 +1,45 @@
+import Exceptions.DeckCountException;
+import Model.Cards.Ace;
+import Model.Cards.Card;
 import Model.Deck.Deck;
 import Model.Deck.ShuffleStrategies.FisherYatesStrategy;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
+import Model.Deck.ShuffleStrategies.ModifiedFisherYatesStrategy;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.Optional;
 import static Model.Constants.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DeckTesting {
 
-    private final Deck deck;
+    private Deck deck;
 
-    public DeckTesting() {
+    @BeforeEach
+    public void setUp() {
         deck = new Deck(DEFAULT_NUMBER_OF_DECKS, new FisherYatesStrategy());
     }
 
-    /** tests that the deck has the correct number of cards given some n copies. */
-    @Order(1)
     @Test
     public void testDeckCardCount() {
         assertEquals(DEFAULT_NUMBER_OF_DECKS * NUMBER_OF_SUITS * NUMBER_OF_CARDS_PER_SUIT,
                 deck.getDeck().size());
     }
 
-    /** tests that a card is successfully popped off of the deck stack after being dealt. */
-    @Order(2)
     @Test
-    public void testDeal() {
+    public void testDealDecrementsDeckSize() {
         deck.deal();
         assertEquals(DEFAULT_NUMBER_OF_DECKS * NUMBER_OF_SUITS * NUMBER_OF_CARDS_PER_SUIT - 1,
                 deck.getDeck().size());
     }
 }
+
+    @Test
+    public void testDealReturnsCard() {
+        Optional<Card> card = deck.deal();
+        assertTrue(card.isPresent());
+        assertNotNull(card.get().getName());
+        assertTrue(card.get().getValue() > 0);
+    }
+
