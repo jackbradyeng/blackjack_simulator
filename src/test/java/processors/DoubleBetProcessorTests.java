fc746c0c5f20892f5e8e5e40b03725ea3944ed24
jack-brady <jackbradyx@proton.me>
2026-04-30 20:56:30 +1000

    Add double bet processor unit tests

---
+++ src/test/java/processors/DoubleBetProcessorTests.java
@@ -0,0 +1,63 @@
+package processors;
+
+import Model.Actors.Player;
+import Model.Table.Bets.Bet;
+import Model.Table.Bets.DoubleBet;
+import Model.Table.Hands.PlayerHand;
+import Model.Table.Positions.PlayerPosition;
+import Model.Table.Processors.DoubleBetProcessors.DoubleBetProcessorImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DoubleBetProcessorTests {
+
+    private DoubleBetProcessorImpl processor;
+    private Player player;
+    private PlayerPosition position;
+    private PlayerHand hand;
+
+    @BeforeEach
+    public void setUp() {
+        processor = new DoubleBetProcessorImpl();
+        player = new Player(500, null);
+        position = new PlayerPosition(1, player);
+        hand = new PlayerHand(position);
+        position.getHands().add(hand);
+        hand.getPairs().add(Map.entry(player, new Bet(50)));
+    }
+
+    @Test
+    public void process_addsDoubleBetToHand() {
+        processor.process(player, position, hand);
+        long doubleBetCount = hand.getPairs().stream()
+                .filter(p -> p.getValue() instanceof DoubleBet)
+                .count();
+        assertEquals(1, doubleBetCount);
+    }
+
+    @Test
+    public void process_doubleBetAmountMatchesOriginalBet() {
+        processor.process(player, position, hand);
+        double doubleBetAmount = hand.getPairs().stream()
+                .filter(p -> p.getValue() instanceof DoubleBet)
+                .findFirst()
+                .orElseThrow()
+                .getValue()
+                .getAmount();
+        assertEquals(50, doubleBetAmount);
+    }
+
+    @Test
+    public void process_deductsOriginalBetAmountFromChips() {
+        processor.process(player, position, hand);
+        assertEquals(450, player.getChips());
+    }
+
+    @Test
+    public void process_handRetainsBothOriginalAndDoubleBet() {
+        processor.process(player, position, hand);
+        assertEquals(2, hand.getPairs().size());
+    }
+}
